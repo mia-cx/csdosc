b3d4d62ea7a2e37ed6abb6ed595dd0bb7fe27afb
Mia <mia@mia.cx>
2021-10-19 10:32:24 +0000

    add bouncing-ball & potential stylesheet

--- assignments/bouncing-ball/environment.js
+++ assignments/bouncing-ball/environment.js
@@ -2,6 +2,7 @@
 export let canvasWidth      = window.innerWidth;
 export let canvasHeight     = window.innerHeight;
 
+<<<<<<< HEAD
 export let frameRate        = 288;
 
 /* Physics */
@@ -13,6 +14,19 @@ export let d = 0;                         // drag / air restistance             
 export let amount           = 25;        // amount of initial balls
 export let size             = 100;        // average size of initial balls
 export let velocity         = 0.1;         // average velocity of initial balls
+=======
+export let frameRate        = 120;
+
+/* Physics */
+export let g = 98.1;                      // gravitational constant               (0 to disable)
+export let b = 0.95;                       // bounciness / restituion coëfficient
+export let d = 0;                         // drag / air restistance               (0 to disable)
+
+/* Object */
+export let amount           = 50;        // amount of initial balls
+export let size             = 100;        // average size of initial balls
+export let velocity         = 3;         // average velocity of initial balls
+>>>>>>> 450d0e5 (add bouncing-ball & potential stylesheet)
 
 export let sizeVariance     = 3;          // variance in size of initial balls
 export let veloVariance     = 10;         // variance in velocity of inital balls
