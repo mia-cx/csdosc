8ef1ce44173e1403ec69f3f43ca6de7066fa285c
Mia <mia@mia.cx>
2021-10-20 15:50:53 +0200

    update gitignore add postinstall.sh

---
+++ postinstall.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# create missing directories
+#   -p                          only create if missing
+mkdir -p public/lib/js
+mkdir -p public/lib/css
+
+# copy js libraries
+#   -u                          update if newer or nonexistent
+#   -r                          recursive
+##   --remove-destination       remove existing dest file before opening
+cp -u node_modules/bootstrap/dist/js/bootstrap.min.js* public/lib/js/
+cp -u node_modules/jquery/dist/jquery.min.js* public/lib/js/
+cp -u node_modules/p5/lib/*.min.js public/lib/js/
+cp -u node_modules/p5/lib/addons/*.min.js public/lib/js/
+cp -u node_modules/socket.io/client-dist/socket.io.min.js* public/lib/js/
+
+# copy css libraries
+cp ./node_modules/bootstrap/dist/css/bootstrap.min.css* ./public/lib/css/
