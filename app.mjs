11982c2bd0095574eb3d57d4f2102e4abd589947
Mia <mia@mia.cx>
2021-10-29 07:52:26 +0200

    streamline the update checker adjust file formatter

--- app.mjs
+++ app.mjs
@@ -1,44 +1,49 @@
-import express from 'express';
+import express from "express";
 const app = express();
 
-import * as readline from 'readline';
-const rl = readline.createInterface({ input: process.stdin, output: process.stdout }) ;
+import * as readline from "readline";
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
 
-import update from './api/update.mjs';
+import update from "./api/update.mjs";
 
 const PORT = process.argv[2] ? process.argv[2] : 8001;
 
 process.on("SIGINT", () => {
-  console.debug("received SIGINT, closing server.");
-  stop("SIGINT");
+    console.debug("received SIGINT, closing server.");
+    stop("SIGINT");
 });
 
 rl.on("line", (input) => {
-  switch (true) {
-    case input.startsWith("kill"):
-    case input.startsWith("stop"):
-    case input.startsWith("quit"):
-    case input.startsWith("q"):
-      stop("stop command");
-      break;
-    case input.startsWith("update"):
-      update(input.split(" ")[1]);
-  }
+    switch (true) {
+        case input.startsWith("kill"):
+        case input.startsWith("stop"):
+        case input.startsWith("quit"):
+        case input.startsWith("q"):
+            stop("stop command");
+            break;
+        case input.startsWith("update"):
+            try {
+                update(input.split(" ")[1]);
+            } catch (e) {
+                console.error(e.stack);
+            }
+    }
 });
 
 rl.on("SIGINT", () => {
-  process.emit("SIGINT");
+    process.emit("SIGINT");
 });
-
-
 
 /**
  * closes the server and exits the process (semi-gracefully)
  * @param {string} reason provided reason for the close/exit
  */
 function stop(reason) {
-  console.log("stopping server!");
-  if (reason) console.log("reason: " + reason);
-  // TODO killOsc()
-  process.exit(0);
+    console.log("stopping server!");
+    if (reason) console.log("reason: " + reason);
+    // TODO killOsc()
+    process.exit(0);
 }
